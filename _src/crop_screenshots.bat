f059be6c17838232c1934bcf8105def9e2e9c239
David Oakley <eso@blott.org>
2025-01-27 11:38:51 +0000

    Add screenshots and a script to crop them

---
+++ _src/crop_screenshots.bat
@@ -0,0 +1,11 @@
+CD screenshots
+
+MD cropped
+
+magick Screenshot_20250127_085348.png -crop 375x790+2185+349 cropped\basic_sidebar.png
+magick Screenshot_20250127_085606.png -crop 375x790+2185+349 cropped\vc_sidebar.png
+magick Screenshot_20250127_090026.png -crop 375x790+2185+349 cropped\zones_sidebar.png
+magick Screenshot_20250127_090302.png -crop 375x790+2185+349 cropped\houses_sidebar.png
+REM magick Screenshot_20250127_090905.png -crop 375x790+2185+349 cropped\el_sidebar.png
+
+magick Screenshot_20250127_091429.png -crop 438x285+0+1096 cropped\er_chat.png
