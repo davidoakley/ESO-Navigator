593b32e0c51c64a43899939ce57a1cbcf79220e4
David Oakley <eso@blott.org>
2025-02-07 16:12:06 +0000

    Updated localisations (en+fr)

--- lang/en.lua
+++ lang/en.lua
@@ -3,24 +3,25 @@ local mkstr = function(id, str)
     SafeAddVersion(id, 1)
 end
 
--- is this needed? mkstr("NAVIGATOR_OPENTAB","Open Navigator tab (on Map screen)")
+-- Controls menu entry (opens the Navigator tab on the World Map)
+mkstr("SI_BINDING_NAME_NAVIGATOR_SEARCH", "Open on World Map")
 
 -- Name of the tab on the World Map
 mkstr("NAVIGATOR_TAB_SEARCH","Navigator")
 
 -- Shown on the bottom keybind bar
-mkstr("NAVIGATOR_KEYBIND_SEARCH", "Search")
+NAVIGATOR_KEYBIND_SEARCH = SI_GAMEPAD_HELP_SEARCH
 
 -- Edit box hint text (<<1>> is replaced by 'Tab')
 mkstr("NAVIGATOR_SEARCH_KEYPRESS","Search (<<1>>)")
 mkstr("NAVIGATOR_SEARCH","Search locations, zones or @players")
 
 -- Category Headings
-mkstr("NAVIGATOR_CATEGORY_RESULTS", "Results")
 mkstr("NAVIGATOR_CATEGORY_BOOKMARKS", "Bookmarks")
 mkstr("NAVIGATOR_CATEGORY_RECENT", "Recent")
 mkstr("NAVIGATOR_CATEGORY_ZONES", "Zones")
-mkstr("NAVIGATOR_CATEGORY_GROUP", "Group")
+NAVIGATOR_CATEGORY_RESULTS = SI_GAMEPAD_TRADING_HOUSE_BROWSE_RESULTS_TITLE
+NAVIGATOR_CATEGORY_GROUP = SI_MAIN_MENU_GROUP
 
 -- Result hints
 mkstr("NAVIGATOR_HINT_NORESULTS", "No results found")
@@ -31,32 +32,30 @@ mkstr("NAVIGATOR_HINT_NOBOOKMARKS", "Right click a destination to create (or del
 mkstr("NAVIGATOR_KEY_ENTER", "Enter")
 
 -- Result types
-mkstr("NAVIGATOR_DUNGEON", "Dungeon")
-mkstr("NAVIGATOR_TRIAL", "Trial")
-mkstr("NAVIGATOR_ARENA", "Arena")
+NAVIGATOR_DUNGEON = SI_GROUPFINDERCATEGORY0
+NAVIGATOR_ARENA = SI_GROUPFINDERCATEGORY1
+NAVIGATOR_TRIAL = SI_GROUPFINDERCATEGORY2
 
 -- Tooltips
-mkstr("NAVIGATOR_NOT_KNOWN", "Not known by this character")
+mkstr("NAVIGATOR_NOT_KNOWN", "Not known by this character") -- Location not known
 
--- Actions
-mkstr("NAVIGATOR_NO_TRAVEL_PLAYER", "No players to travel to")
+-- Action and menu items
 mkstr("NAVIGATOR_TRAVEL_TO_ZONE", "Travel to <<1>>")
-mkstr("NAVIGATOR_CANNOT_TRAVEL_TO_PLAYER", "Unable to travel to player <<1>>")
+mkstr("NAVIGATOR_MENU_ADDBOOKMARK", "Add Bookmark")
+mkstr("NAVIGATOR_MENU_ADDHOUSEBOOKMARK", "Add Primary Residence Bookmark")
+mkstr("NAVIGATOR_MENU_REMOVEBOOKMARK", "Remove Bookmark")
+NAVIGATOR_MENU_SHOWONMAP = SI_QUEST_JOURNAL_SHOW_ON_MAP
+NAVIGATOR_MENU_SETDESTINATION = SI_WORLD_MAP_ACTION_SET_PLAYER_WAYPOINT
 
--- Chat box output
+-- Status / error messages
+mkstr("NAVIGATOR_NO_TRAVEL_PLAYER", "No players to travel to")
+mkstr("NAVIGATOR_CANNOT_TRAVEL_TO_PLAYER", "Unable to travel to player <<1>>")
 mkstr("NAVIGATOR_NO_PLAYER_IN_ZONE", "Failed to find a player to travel to in <<1>>")
 mkstr("NAVIGATOR_PLAYER_NOT_IN_ZONE", "<<1>> is no longer in <<2>>")
-mkstr("NAVIGATOR_TRAVELING_TO_ZONE_VIA_PLAYER", "Traveling to <<1>> via <<2>>")
+mkstr("NAVIGATOR_TRAVELING_TO_ZONE_VIA_PLAYER", "Traveling to <<1>> via <<2>>") -- 1:player 2:zone
 
 -- Chat slash command
 mkstr("NAVIGATOR_SLASH_DESCRIPTION", "Navigator: Teleports to the given zone, wayshrine, house or player")
 
--- Menu items
-mkstr("NAVIGATOR_MENU_ADDBOOKMARK", "Add Bookmark")
-mkstr("NAVIGATOR_MENU_ADDHOUSEBOOKMARK", "Add Primary Residence Bookmark")
-mkstr("NAVIGATOR_MENU_REMOVEBOOKMARK", "Remove Bookmark")
-mkstr("NAVIGATOR_MENU_SHOWONMAP", "Show on map")
-mkstr("NAVIGATOR_MENU_SETDESTINATION", "Set Destination")
-
--- Location names
+-- Custom location names
 mkstr("NAVIGATOR_LOCATION_OBLIVIONPORTAL", "Oblivion Portal")
--- lang/fr.lua
+++ lang/fr.lua
@@ -2,38 +2,60 @@ local mkstr = function(id, str)
     SafeAddString(_G[id], str, 1)
 end
 
--- Shown on the bottom keybind bar
-mkstr("NAVIGATOR_KEYBIND_SEARCH", "Rechercher")
+
+-- Controls menu entry (opens the Navigator tab on the World Map)
+-- mkstr("SI_BINDING_NAME_NAVIGATOR_SEARCH", "Open on World Map")
+
+
+
+
+
+
 
 -- Edit box hint text (<<1>> is replaced by 'Tab')
+mkstr("NAVIGATOR_SEARCH_KEYPRESS", "Rechercher (<<1>>)") -- 1:Keyname
 mkstr("NAVIGATOR_SEARCH","Rechercher des lieux / zones / @joueurs")
-mkstr("NAVIGATOR_SEARCH_KEYPRESS", "Rechercher (<<1>>)")
 
 -- Category Headings
-mkstr("NAVIGATOR_CATEGORY_RESULTS", "Résultats")
 mkstr("NAVIGATOR_CATEGORY_BOOKMARKS", "Signets")
 mkstr("NAVIGATOR_CATEGORY_RECENT", "Récents")
 mkstr("NAVIGATOR_CATEGORY_ZONES", "Zones")
 
+
+
+-- Result hints
+--mkstr("NAVIGATOR_HINT_NORESULTS", "No results found")
+--mkstr("NAVIGATOR_HINT_NORECENTS", "Destinations that you travel to will automatically appear here")
+--mkstr("NAVIGATOR_HINT_NOBOOKMARKS", "Right click a destination to create (or delete) a bookmark for it")
+
 -- Enter key label (keep it short!)
 mkstr("NAVIGATOR_KEY_ENTER", "Entrée")
 
--- Result types
-mkstr("NAVIGATOR_DUNGEON", "Donjon")
-mkstr("NAVIGATOR_TRIAL", "Épreuve")
-mkstr("NAVIGATOR_ARENA", "Arène")
+
+
+
+
 
 -- Tooltips
--- mkstr("NAVIGATOR_NOT_KNOWN", "Not known by this character")
+-- mkstr("NAVIGATOR_NOT_KNOWN", "Not known by this character") -- Location not known
 
--- Actions
--- mkstr("NAVIGATOR_NO_TRAVEL_PLAYER", "No players to travel to")
-mkstr("NAVIGATOR_TRAVEL_TO_ZONE", "Se rendre à <<1>>")
+-- Actions items
+mkstr("NAVIGATOR_TRAVEL_TO_ZONE", "Se rendre à <<1>>") -- 1:Zone
+--mkstr("NAVIGATOR_MENU_ADDBOOKMARK", "Add Bookmark")
+--mkstr("NAVIGATOR_MENU_ADDHOUSEBOOKMARK", "Add Primary Residence Bookmark")
+--mkstr("NAVIGATOR_MENU_REMOVEBOOKMARK", "Remove Bookmark")
 
--- Chat box output
--- mkstr("NAVIGATOR_NO_PLAYER_IN_ZONE", "Failed to find a player to travel to in <<1>>")
--- mkstr("NAVIGATOR_PLAYER_NOT_IN_ZONE", "<<1>> is no longer in <<2>>")
--- mkstr("NAVIGATOR_TRAVELING_TO_ZONE_VIA_PLAYER", "Traveling to <<1>> via <<2>>")
+
+
+-- Status / error messages
+--mkstr("NAVIGATOR_NO_TRAVEL_PLAYER", "No players to travel to")
+--mkstr("NAVIGATOR_CANNOT_TRAVEL_TO_PLAYER", "Unable to travel to player <<1>>") -- 1:player
+--mkstr("NAVIGATOR_NO_PLAYER_IN_ZONE", "Failed to find a player to travel to in <<1>>") -- 1:zone
+--mkstr("NAVIGATOR_PLAYER_NOT_IN_ZONE", "<<1>> is no longer in <<2>>") -- 1:player 2:zone
+--mkstr("NAVIGATOR_TRAVELING_TO_ZONE_VIA_PLAYER", "Traveling to <<1>> via <<2>>") -- 1:player 2:zone
 
 -- Chat slash command
 -- mkstr("NAVIGATOR_SLASH_DESCRIPTION", "Navigator: Teleports to the given zone, wayshrine, house or player")
+
+-- Custom location names
+--mkstr("NAVIGATOR_LOCATION_OBLIVIONPORTAL", "Oblivion Portal")
