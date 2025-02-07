8ba8e05483e72981ddc73de5da060729a7cf9110
David Oakley <eso@blott.org>
2025-02-07 14:55:59 +0000

    Add specific 48x48 versions of most commonly used icons
    Set icon size to 24x24

--- _src/convert_textures.bat
+++ _src/convert_textures.bat
@@ -1,3 +1,7 @@
 REM Download the texconv utility from https://github.com/Microsoft/DirectXTex/wiki/Texconv
 
-texconv -o ..\media -r -y -ft dds -f DXT5 *.png
+texconv -o ..\media -r -y -ft dds -f DXT5 -m 2 icons-assets\*.png
+texconv -o ..\media -r -y -ft dds -f DXT5 -m 2 tabicon-assets\*.png
+texconv -o ..\media -r -y -ft dds -f DXT5 -m 2 bookmark-assets\*.png
+texconv -o ..\media -r -y -ft dds -f DXT5 -m 2 city_narrow.png
+texconv -o ..\media -r -y -ft dds -f DXT5 -m 2 town_narrow.png
