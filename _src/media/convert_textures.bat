197cc8926506c4379f197991b4aac07fb9c4f58b
David Oakley <eso@blott.org>
2025-03-29 09:44:16 +0000

    Move media source into a subdirectory

---
+++ _src/media/convert_textures.bat
@@ -0,0 +1,14 @@
+REM Download the texconv utility from https://github.com/Microsoft/DirectXTex/wiki/Texconv
+
+texconv -o ..\..\media -r -y -ft dds -f DXT5 -m 2 icons-assets\*.png
+texconv -o ..\..\media -r -y -ft dds -f DXT5 -m 2 tabicon-assets\*.png
+texconv -o ..\..\media -r -y -ft dds -f DXT5 -m 2 city_narrow.png
+texconv -o ..\..\media -r -y -ft dds -f DXT5 -m 2 town_narrow.png
+
+if not exist "..\..\media\tags" mkdir ""..\..\media\tags"
+texconv -o ..\..\media\tags -r -y -ft dds -f DXT5 -m 2 tag-icons-assets\*.png
+
+if not exist "..\..\media\overlays" mkdir ""..\..\media\overlays"
+texconv -o ..\..\media\overlays -r -y -ft dds -f DXT5 -m 2 overlays-assets\*.png
+
+DEL ..\..\media\tabicon_large.dds
